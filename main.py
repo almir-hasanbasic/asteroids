40d5be52cae723d678880784bc0c6037feb19f70
almir-hasanbasic <hasanbasic.aff@gmail.com>
2025-10-24 20:28:55 +0200

    Add bullets system to the game

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import pygame
+import sys
+
 from constants import *
 from player import *
 from asteroid import *
 from asteroidfield import *
-import sys
+from shot import *
+
 
 def main():
     pygame.init()
@@ -11,12 +14,14 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
     dt = 0
     x = SCREEN_WIDTH / 2
     y = SCREEN_HEIGHT / 2
     Player.containers = (updatable, drawable)
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = (updatable,)
+    Shot.containers = (updatable, drawable)
     asteroidfield = AsteroidField()
     player = Player(x, y)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
--- player.py
+++ player.py
@@ -1,6 +1,7 @@
 import pygame
 from circleshape import CircleShape
 from constants import *
+from shot import *
 
 
 class Player(CircleShape):
@@ -32,8 +33,15 @@ class Player(CircleShape):
         if keys[pygame.K_w]:
             self.move(dt)
         if keys[pygame.K_s]:
-            self.move(-dt)   
+            self.move(-dt)
+        if keys[pygame.K_SPACE]:
+            self.shoot()
+        
 
     def move(self, dt):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         self.position += forward * PLAYER_SPEED * dt
+
+    def shoot(self):
+        shot = Shot(self.position.x, self.position.y)
+        shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
